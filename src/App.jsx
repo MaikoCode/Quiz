ee93538fa232f582e82b2b07e9cb89e83d2f1f39
Maikoke <mamadou.maikoke@webpick.net>
2023-12-15 18:21:55 +0100

    Add NotFoundPage component to handle 404 errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Homepage from "./Pages/Homepage"
 import Quizpage from "./Pages/Quizpage"
 import Resultspage from "./Pages/Resultspage"
+import NotFoundPage from "./Pages/NotFoundPage"
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import "./App.css"
 
@@ -11,6 +12,7 @@ function App() {
         <Route path="/"  element={<Homepage />}/>
         <Route path="/quiz/:id" element={<Quizpage />} />
         <Route path="/results" element={<Resultspage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </Router>
   )
---
+++ src/Pages/NotFoundPage.jsx
@@ -0,0 +1,9 @@
+import TrollFace from "../assets/images/troll_face.png"
+
+export default function NotFoundPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+        <img src={TrollFace} alt="image" className="w-[500px] h-[500px]"/>
+    </div>
+  )
+}
